948b5916566d52590fade9a96385819a05a146e1
onabz <ONABOLUMBE@GMAIL.COM>
2022-08-08 13:41:07 +0000

    Add Article model to the models.py file

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+STATUS = ((0, "Draft"), (1, "Published"))
+
+class Article(models.Model):
+    title = models.CharField(max_length=255, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_article') 
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+    body = model.TextField()
+    featured_image = CloudinaryField('image', default = 'placeholder')
+    slug = models.SlugField(max_length=255, unique=True)
+    excerpt = models.TextField(blank=True)
+    likes = models.ManyToManyField(User, related_name='article_likes', blank=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    class Meta:
+        ordering = ['-date_created']
+
+    def __str__(self):
+        return self.title
+
+    def number_of_likes(self):
+        return self.likes.count()
+
+
