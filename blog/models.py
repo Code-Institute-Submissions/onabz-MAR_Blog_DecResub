c16c47345a1a4fea53c2897ab71f43b10628b5ed
onabz <ONABOLUMBE@GMAIL.COM>
2022-08-08 14:44:18 +0000

    Add Comment model to the models.py file

--- blog/models.py
+++ blog/models.py
@@ -6,11 +6,11 @@ STATUS = ((0, "Draft"), (1, "Published"))
 
 class Article(models.Model):
     title = models.CharField(max_length=255, unique=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_article') 
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='articles') 
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
-    body = model.TextField()
-    featured_image = CloudinaryField('image', default = 'placeholder')
+    body = models.TextField()
+    featured_image = CloudinaryField('image', default='placeholder')
     slug = models.SlugField(max_length=255, unique=True)
     excerpt = models.TextField(blank=True)
     likes = models.ManyToManyField(User, related_name='article_likes', blank=True)
@@ -26,3 +26,15 @@ class Article(models.Model):
         return self.likes.count()
 
 
+class Comment(models.Model):
+    post = models.ForeignKey(Article, related_name='comments', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    body = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['date_created']
+
+    def __str__(self):
+        return '%s - %s' % (self.article.title, self.name)
