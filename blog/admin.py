e43095f4fc00732c74d415733f44b22d2d711185
onabz <ONABOLUMBE@GMAIL.COM>
2022-08-10 02:01:28 +0000

    Add some configurations to the admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -2,9 +2,12 @@ from django.contrib import admin
 from .models import Article
 from django_summernote.admin import SummernoteModelAdmin
 
+
 @admin.register(Article)
 class ArticleAdmin(SummernoteModelAdmin):
 
+    prepopulated_fields = {'slug': ('title',)}
+    list_display = ('title', 'slug', 'date_created', 'status')
+    search_fields = ('title', 'body')
+    list_filter = ('date_created', 'status')
     summernote_fields = ('body')
-
-
