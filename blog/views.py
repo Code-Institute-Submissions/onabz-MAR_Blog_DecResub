e11a71f25f473875b6ff12235ac73a7e6f64abae
onabz <ONABOLUMBE@GMAIL.COM>
2022-08-11 05:29:18 +0000

    Add ArticleView class to views.py

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Article
 
-# Create your views here.
+
+class ArticleView(generic.ListView):
+    model = Article
+    template_name = 'index.html'
+    queryset = Article.objects.filter(status=1).order_by('-date_created')
