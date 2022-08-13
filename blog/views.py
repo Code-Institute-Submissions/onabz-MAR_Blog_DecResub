66eab698b603993b3df05a35b9aff1f2bc485bcd
onabz <ONABOLUMBE@GMAIL.COM>
2022-08-13 16:04:29 +0000

    Add ArticleDetailView and code for pagination

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Article
 
 
@@ -15,3 +15,23 @@ class AboutView(generic.CreateView):
     template_name = 'about.html'
     fields = '__all__'
 
+
+class ArticleDetailView(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Article.objects.filter(status=1)
+        article = get_object_or_404(queryset, slug=slug)
+        comments = article.comments.filter(approved=True).order_by('date_created')
+        liked = False
+        if article.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        return render(
+            request,
+            "article.html",
+            {
+                "article": article,
+                "comments": comments,
+                "liked": liked
+            }
+        )
