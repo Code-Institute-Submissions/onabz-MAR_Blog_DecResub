f6dfca67b0a6855ff547980985ab2d943adeb71c
onabz <ONABOLUMBE@GMAIL.COM>
2022-08-14 20:02:35 +0000

    make changes to templates and add path for ArticleDetailView

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ from django.urls import path
 urlpatterns = [
     path('', views.ArticleView.as_view(), name='home'),
     path('about/', views.AboutView.as_view(), name='about'),
+    path('<slug:slug>/', views.ArticleDetailView.as_view(), name='article_detail'),
 ]
