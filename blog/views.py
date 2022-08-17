2b859420f2e387d8ef475423f35e88af49a7427c
onabz <ONABOLUMBE@GMAIL.COM>
2022-08-17 02:55:55 +0000

    Add post method to ArticleDetailView class

--- blog/views.py
+++ blog/views.py
@@ -33,6 +33,40 @@ class ArticleDetailView(View):
             {
                 "article": article,
                 "comments": comments,
+                "commented": False,
+                "liked": liked,
+                "comment_form": CommentForm()
+            }
+        )
+
+
+    def post(self, request, slug, *args, **kwargs):
+        queryset = Article.objects.filter(status=1)
+        article = get_object_or_404(queryset, slug=slug)
+        comments = article.comments.filter(approved=True).order_by('date_created')
+        liked = False
+        if article.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        comment_form = CommentForm(data=request.POST)
+
+        if comment_form.is_valid():
+            comment_form.instance.email = request.user.email
+            comment_form.instance.name = request.user.username
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+        else:
+            comment_form = CommentForm()
+
+
+        return render(
+            request,
+            "article.html",
+            {
+                "article": article,
+                "comments": comments,
+                "commented": True,
                 "liked": liked,
                 "comment_form": CommentForm()
             }
