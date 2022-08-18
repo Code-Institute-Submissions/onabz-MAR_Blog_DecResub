d36d23765c26aa1927515678d635640cc254bb98
onabz <ONABOLUMBE@GMAIL.COM>
2022-08-18 14:24:44 +0000

    Change about path name to about_view

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.ArticleView.as_view(), name='home'),
-    path('about/', views.AboutView.as_view(), name='about'),
+    path('about/', views.AboutView.as_view(), name='about_view'),
     path('<slug:slug>/', views.ArticleDetailView.as_view(), name='article_detail'),
 ]
