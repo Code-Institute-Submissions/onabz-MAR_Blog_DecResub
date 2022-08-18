cceaca9d9405899f10726d291319a17c3b0a7519
onabz <ONABOLUMBE@GMAIL.COM>
2022-08-18 12:58:40 +0000

    Import About from .models

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
-from .models import Article
+from .models import Article, About
 from .forms import CommentForm
 
 
