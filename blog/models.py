394c2408bdd1a4842bb3939939df23648372f9c0
onabz <ONABOLUMBE@GMAIL.COM>
2022-08-19 21:39:09 +0000

    Define function get_absolute_url and import reverse

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
+from django.urls import reverse
 
 STATUS = ((0, "Draft"), (1, "Published"))
 
@@ -24,6 +25,9 @@ class Article(models.Model):
 
     def number_of_likes(self):
         return self.likes.count()
+
+    def get_absolute_url(self):
+        return reverse('article_detail', args=[slug])
 
 
 class Comment(models.Model):
