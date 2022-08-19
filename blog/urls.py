f2ab7c3ae4d214316556854fb63522f4ae4f5af8
onabz <ONABOLUMBE@GMAIL.COM>
2022-08-19 18:37:08 +0000

    Add css style sheet link

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,7 @@ from django.urls import path
 urlpatterns = [
     path('', views.ArticleView.as_view(), name='home'),
     path('about/', views.AboutView.as_view(), name='about_view'),
-    path('add-article/', views.AddArticleView.as_view(), name='add'),
+    path('add_article/', views.AddArticleView.as_view(), name='add'),
     path('<slug:slug>/', views.ArticleDetailView.as_view(), name='article_detail'),
     path('like/<slug:slug>', views.ArticleLike.as_view(), name='article_like'),
 ]
