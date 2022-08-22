893229e4077930540541f52a47741e92f5a6ad33
onabz <ONABOLUMBE@GMAIL.COM>
2022-08-22 07:54:52 +0000

    Apply summernote to forms.py

--- blog/forms.py
+++ blog/forms.py
@@ -1,8 +1,14 @@
 from django import forms
 from .models import Comment
+from django_summernote.widgets import SummernoteWidget, SummernoteInplaceWidget
+
 
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('body',)
+
+
+class AnotherForm(forms.Form):
+    bar = forms.CharField(widget=SummernoteInplaceWidget())
